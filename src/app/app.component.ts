ee774c80ce8a16ec646b2eb84edcbb5004893deb
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-25 20:45:16 +0530

    Added:Code to launch New Site in a new Tab on clicking logo.

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'HelloWorld';
+  imgUrl="../assets/Bridgelabz.png";
+  url="https://www.w3schools.com/";
+
+  ngOnInit(): void{
+    this.title="Hello From Bridgelabz.";
+  }
+
+  OnClick($event){
+    console.log("Save Button is clicked",$event);
+    window.open(this.url,"_blank");
+  }
+}
